45e5da5f28227e016302ca4810732a8729e25d23
LaVida007101 <gdumallay007101@gmail.com>
2025-03-17 05:17:50 +0800

    Redirected site to briefcase profile

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   env: {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://edith.feutech.edu.ph/briefcase/profile/genesisdumallay",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
