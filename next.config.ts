ec12c9c784e91e7cb3ed67c1388e1fcaf0e7540a
Genesis M. Dumallay <gdumallay007101@gmail.com>
2025-04-23 00:20:12 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
